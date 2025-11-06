c815f602189c5780db436a01d07d5e819442af18
38327014 <38327014@L815WD220013.edu.cbe.ab.ca>
2025-11-06 15:16:32 -0700

    s

--- Chaper11/src/SkillBuilders/StudentData.java
+++ Chaper11/src/SkillBuilders/StudentData.java
@@ -3,10 +3,16 @@ package SkillBuilders;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JTextField;
+import java.awt.GridLayout;
 
 public class StudentData {
 
 	private JFrame frame;
+	private JTextField txtEnterStudentName;
+	private JTextField txtEnterStudentScore;
 
 	/**
 	 * Launch the application.
@@ -36,8 +42,28 @@ public class StudentData {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 287, 155);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setLayout(new GridLayout(0, 1, 0, 0));
+		
+		txtEnterStudentName = new JTextField();
+		txtEnterStudentName.setText("Enter Student Name:");
+		panel.add(txtEnterStudentName);
+		txtEnterStudentName.setColumns(10);
+		
+		txtEnterStudentScore = new JTextField();
+		txtEnterStudentScore.setText("Enter Student Score:");
+		txtEnterStudentScore.setColumns(10);
+		panel.add(txtEnterStudentScore);
 	}
 
+	public JTextField getTxtEnterStudentName() {
+		return txtEnterStudentName;
+	}
+	public JTextField getTxtEnterStudentScore() {
+		return txtEnterStudentScore;
+	}
 }
